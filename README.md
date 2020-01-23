b214337bf68febd788a03ddd99bc8aa8609558fb
softAdd <soft.snake867@gmail.com>
2020-01-23 19:48:01 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#NASA-GALERY
+
+The project built with Sass, JS and Webpack
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.<br>
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br>
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `dist` folder.
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.[chunkhash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: './src/public/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ]
+}
