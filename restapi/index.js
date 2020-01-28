92331bca52b06cfc94b07620bc0aef9ee434ee75
softAdd <soft.snake867@gmail.com>
2020-01-28 23:07:21 +0500

    add docker for restapi + docker-compose

--- restapi/index.js
+++ restapi/index.js
@@ -2,4 +2,28 @@ const app = require('./app')
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server has been started on ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Server has been started on ${PORT}`))
+
+// shut down server
+function shutdown() {
+    server.close(function onServerClosed(err) {
+        if (err) {
+            console.error(err)
+            process.exitCode = 1
+        }
+        process.exit()
+    })
+}
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint() {
+    console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString())
+    shutdown()
+})
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm() {
+    console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString())
+    shutdown()
+})
+
