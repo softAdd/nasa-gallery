b54f817b5c6b86096eb6772fdd088f5fce405da0
softAdd <soft.snake867@gmail.com>
2020-02-02 14:19:26 +0500

    salt

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+const bcrypt = require('bcrypt')
 const PouchDB = require('../pouchdb')
 const { COUCHDB_URL } = process.env
 const nasaUsersDB = new PouchDB(`${COUCHDB_URL}/nasa_users`, {
@@ -10,29 +12,51 @@ const checkLoginRequest = (req, res) => {
     if (!req.body.password) return res.status(400).json({ error: errorTypes.requiredPassword })
 }
 
-module.exports.login = function (req, res) {
+module.exports.login = async function (req, res) {
     checkLoginRequest(req, res)
 
     res.sendStatus(200)
 }
 
-module.exports.register = function (req, res) {
+module.exports.register = async function (req, res) {
     checkLoginRequest(req, res)
 
-    nasaUsersDB.find({
-        selector: {
-            email: req.body.email
-        },
-        fields: ["email"],
-        limit: 1
-    }).then(result => {
-        res.status(200).json({
-            result,
-            exampleDocs: result.docs
+    try {
+        const { docs } = await nasaUsersDB.find({
+            selector: {
+                email: req.body.email
+            },
+            limit: 1
         })
-    }).catch(error => {
-        res.status(200).json({
-            error
+        if (docs > 0) {
+            res.status(409).json({
+                message: 'email is busy'
+            })
+        } else {
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) throw new Error('Error while generating salt')
+
+                bcrypt.hash(req.body.password, salt, async function(err, hash) {
+                    if (err) throw new Error('Error while generating hash')
+
+                    const user = {
+                        _id: `${req.body.email}-${moment().format("DD.MM.YYYY-hh:mm:ss")}`,
+                        email: req.body.email,
+                        password: hash,
+                        savedImages: []
+                    }
+
+                    await nasaUsersDB.put(user, err => {
+                        if (err) throw new Error('Error while putting into database')
+
+                        res.sendStatus(201)
+                    })
+                })
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            error: err
         })
-    })
+    }
 }
